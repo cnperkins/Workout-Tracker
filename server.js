a4af906f6d7702cd24c93275217b1f38d3e9ceda
chlokins <cnpolansky@gmail.com>
2020-09-24 20:05:55 -0500

    deploying

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 require("./routes/api-routes.js")(app);
 
